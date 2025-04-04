5327a809475db1f35e6f57eb1766992fca37c6e8
Rajvish3133 <rv2477207@gmail.com>
2025-04-04 16:51:15 +0530

    Create Rotate_Array.cpp

---
+++ medium1/Rotate_Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) { 
+    vector<int>ans;
+    int n=nums.size();
+    for(int i=0;i<n;i++){
+      ans.push_back(nums[i]);
+    } for(int i=0;i<n;i++){
+        nums[(i+k)%n]=ans[i];
+    }
+    }
+};
